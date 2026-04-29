9f6521025eb932d78f0087f4fe545efde3c65c25
Simon Heiss <terentevsa002@gmail.com>
2026-04-29 05:59:24 +0300

    back init

---
+++ back/src/main/java/com/simonheiss/back/controllers/TestController.java
@@ -0,0 +1,18 @@
+package com.simonheiss.back.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+    private final Random rand = new Random();
+
+    @GetMapping("/getNum")
+    public int getNum(){
+        return rand.nextInt(1000);
+    }
+}
